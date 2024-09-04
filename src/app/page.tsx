fd90dd4f013112d94518c9deb0564c2d846e0189
Avi <avibanerjee2000@gmail.com>
2024-09-04 23:38:29 +0530

    created different modules for navbar, plans, and periods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
+import Navbar from './_components/Navbar';
+import Plans from "./_components/Plans";
+import Periods from "./_components/Periods";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      <div></div>
-      <div className="mt-auto mb-20">
-        <div className="flex p-3 justify-between gap-5 border rounded-full">
-          <div className="py-1 px-3 rounded-full text-center my-auto cursor-pointer hover:bg-slate-400">Day</div>
-          <div className="py-1 px-3 rounded-full text-center my-auto cursor-pointer hover:bg-slate-400">Week</div>
-          <div className="py-1 px-3 rounded-full text-center my-auto cursor-pointer hover:bg-slate-400">Month</div>
-          <div className="py-1 px-3 rounded-full text-center my-auto cursor-pointer hover:bg-slate-400">Year</div>
-        </div>
-      </div>
+      <Navbar />
+      <Plans />
+      <Periods />
     </main>
   );
 }
